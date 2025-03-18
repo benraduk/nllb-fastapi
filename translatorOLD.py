87934ab2fc7f33f5ab9ca4c4a28108d3677f18ce
Ben Radcliffe <ben@braiv.co>
2025-03-18 19:36:31 +1100

    new translator

--- translatorOLD.py
+++ translatorOLD.py
@@ -2,31 +2,33 @@ import ctranslate2
 import transformers
 from lang_list import lang_list
 
-class Translator:
+
+class translator:
     def __init__(self, model_dir, tokenizer_dir):
         """
         Initialize the translator class with the model and tokenizer directories.
         """
         self.translator = ctranslate2.Translator(model_dir, device="cuda")
-        self.tokenizer_dir = tokenizer_dir  # Store tokenizer directory
         self.lang_list = lang_list
 
     def translate(self, src_lang, tgt_lang, input_text):
         """
         Translate the input text from the source language to the target language.
         """
-        # Use the correct tokenizer directory
+
         tokenizer = transformers.AutoTokenizer.from_pretrained(
-            self.tokenizer_dir, src_lang=src_lang
+            tokenizer_dir, src_lang=src_lang
         )
 
         input_tokens = tokenizer.convert_ids_to_tokens(tokenizer.encode(input_text))
+
         target_prefix = [tgt_lang]
 
         results = self.translator.translate_batch(
             [input_tokens], target_prefix=[target_prefix]
         )
         target = results[0].hypotheses[0][1:]
+
         output = tokenizer.decode(tokenizer.convert_tokens_to_ids(target))
 
         return output
@@ -38,6 +40,7 @@ class Translator:
         invalid_languages = []
         if src_lang not in self.lang_list:
             invalid_languages.append(src_lang)
+
         if tgt_lang not in self.lang_list:
             invalid_languages.append(tgt_lang)
 
@@ -47,4 +50,7 @@ class Translator:
         """
         Check if the source and target languages are not the same.
         """
-        return src_lang != tgt_lang
+
+        if src_lang == tgt_lang:
+            return False
+        return True
